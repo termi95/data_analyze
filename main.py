84b8a3d58855936887bde2b11ede1789f741293e
Michał Krzysteczko <m.krzysteczko@icloud.com>
2024-11-11 09:51:30 +0100

    footer, css improvements

--- main.py
+++ main.py
@@ -2,13 +2,14 @@ from flask import Flask, render_template
 from db import recreate_db, get_data
 
 app = Flask(__name__)
+app.config['base_url'] = "http://127.0.0.1:5000"
 
 @app.route("/")
 def hello_world():
     recreate_db()
-    return render_template("index.html")
+    return render_template("index.html", base_url=app.config['base_url'])
 
 @app.route("/data")
 def data():
     get_data()
-    return render_template("data.html")
+    return render_template("data.html", base_url=app.config['base_url'])
